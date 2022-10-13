9c7cd212f45d27d57f18e6971ac7d52f24c21896
vaddadiPhani <vphanibharath34@gmail.com>
2022-10-14 01:56:08 +0530

    Nav Bar

---
+++ navbarforshopperstop/scripts/navbar.js
@@ -0,0 +1,11 @@
+// function newconatiner(){
+//     document.getElementById('luxeonhover').innerHTML = ''; 
+//     var maindiv = document.createElement('div');
+//     var div1 = document.createElement('div');
+//     div1.innerText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+//     document.getElementById('luxeonhover').append(div1);
+
+
+// }
+let color1 = document.getElementById('luxe2345');
+color1.style.color = '#D3A84F';
